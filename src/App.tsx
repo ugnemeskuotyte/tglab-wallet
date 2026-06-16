745d91380ba3f78fcebf30f4ef086dad16f758ce
Ugne Meskuotyte <ugnemeskuotyte@Ugnes-MBP.lan>
2026-06-16 15:20:23 +0300

    Routing and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
-function App() {
-  return <div>Wallet App</div>
-}
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { ThemeProvider } from './context/ThemeContext'
+import PrivateRoute from './routes/PrivateRoute'
+import PublicRoute from './routes/PublicRoute'
+import { Layout } from './components'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
+import BetsPage from './pages/BetsPage'
+import TransactionsPage from './pages/TransactionsPage'
 
-export default App
+export default function App() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PublicRoute />}>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Route>
+            <Route element={<PrivateRoute />}>
+              <Route element={<Layout />}>
+                <Route path="/" element={<DashboardPage />} />
+                <Route path="/bets" element={<BetsPage />} />
+                <Route path="/transactions" element={<TransactionsPage />} />
+              </Route>
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  )
+}
